310e9ce89f75a82bf79607db1165386bc519c4cf
maxuan <maxuan123456>
2024-06-26 13:41:47 +0800

    init projct

---
+++ src/main/java/com/max/ai/MaxAiApplication.java
@@ -0,0 +1,13 @@
+package com.max.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxAiApplication.class, args);
+    }
+
+}
